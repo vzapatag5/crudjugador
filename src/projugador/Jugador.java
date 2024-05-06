028d6dd3d0c035a2bbe7f4e5b70966c10e5cf464
vzapatag5 <vzapatag5@eafit.edu.co>
2024-05-05 22:15:15 -0500

    version 1

---
+++ src/projugador/Jugador.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projugador;
+public class Jugador {
+   String id;
+   String nombre;
+   String nivel;
+   String juego;
+   String score;
+
+   public Jugador(){
+   }
+   
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getJuego() {
+        return juego;
+    }
+
+    public void setJuego(String juego) {
+        this.juego = juego;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+   
+   
+}
---
+++ src/projugador/Projugador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projugador;
+
+public class Projugador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //instanciar objeto del formulario formJugador
+        formJugador player = new formJugador();
+        player.setVisible(true);
+        Jugador jugador = new Jugador();
+        // TODO code application logic here
+    }
+    
+}
